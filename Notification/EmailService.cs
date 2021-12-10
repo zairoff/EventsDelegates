dd76fcdc0c69ca66846a7075e751bca71bfbf02a
zairoff <marufzoiroff@gmail.com>
2021-12-10 15:17:15 +0500

    refactored: added Notification library. Removed External library.

---
+++ Notification/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification
+{
+    public class EmailService : INotificationService
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending email: {message}");
+        }
+    }
+}
---
+++ Notification/SmsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification
+{
+    public class SmsService : INotificationService
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending sms... {message}");
+        }
+    }
+}

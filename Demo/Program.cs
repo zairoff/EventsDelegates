b466d9cf50a99a1c15ca95dddf394ea5126da46a
zairoff <marufzoiroff@gmail.com>
2021-12-10 10:34:10 +0500

    refactored

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -9,21 +9,18 @@ namespace Demo
         static void Main(string[] args)
         {
             var videoEncoder = new VideoEncoder();
+            var audioEncoder = new AudioEncoder();
+
             var emailService = new EmailService();
             var smsService = new SmsService();
+
             videoEncoder.Encoded += emailService.Send;
             videoEncoder.Encoded += smsService.Send;
-
-            videoEncoder.Encode(new byte[10]);
-
-            var audioEncoder = new AudioEncoder();
             audioEncoder.Encoded += emailService.Send;
             audioEncoder.Encoded += smsService.Send;
 
+            videoEncoder.Encode(new byte[10]);         
             audioEncoder.Encode(null);
-
-
-            //Console.WriteLine("Hello World!");
         }
     }
 }

39fb86a5ed3d388ab9bc67e40a2bb0824753a42b
Maruf <marufzoiroff@gmail.com>
2021-12-13 10:59:54 +0500

    added factory pattern and implemented

---
+++ Encoder/Factory/AudioFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encoder.Factory
+{
+    public class AudioFactory : EncoderFactory
+    {
+        public override BaseEncoder GetEncoder()
+        {
+            return new AudioEncoder();
+        }
+    }
+}
---
+++ Encoder/Factory/VideoFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encoder.Factory
+{
+    public class VideoFactory : EncoderFactory
+    {
+        public override BaseEncoder GetEncoder()
+        {
+            return new VideoEncoder();
+
+        }
+    }
+}

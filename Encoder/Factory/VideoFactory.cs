1c2b4d90fd7dfb4be24cb6bf56271ee5e7cad918
Maruf <marufzoiroff@gmail.com>
2021-12-14 11:20:53 +0500

    cleaned up all unnecessary using statements

--- Encoder/Factory/VideoFactory.cs
+++ Encoder/Factory/VideoFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Encoder.Factory
+﻿namespace Encoder.Factory
 {
     public class VideoFactory : EncoderFactory
     {
